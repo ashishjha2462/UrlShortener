e300a163b733647008d64b506b01b986b30bd8c3
Ashish Jha <96326894+ashishjha2462@users.noreply.github.com>
2025-07-10 17:18:14 +0530

    Update Url.js

--- models/Url.js
+++ models/Url.js
@@ -4,8 +4,8 @@ const urlSchema = new mongoose.Schema({
   originalUrl: { type: String, required: true },
   shortCode: { type: String, required: true, unique: true },
   createdAt: { type: Date, default: Date.now },
-  expiresAt: { type: Date }, // optional
-  clickCount: { type: Number, default: 0 } // for analytics (bonus)
+  expiresAt: { type: Date },
+  clickCount: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model("Url", urlSchema);
