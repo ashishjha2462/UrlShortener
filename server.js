5daadfe314b963b2743b68f5937f5ce343244b0f
Ashish Jha <96326894+ashishjha2462@users.noreply.github.com>
2025-07-11 14:07:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ app.use(limiter);
 
 app.use("/", urlRoutes);
 
-app.get('/', (req, res) => {
-  res.send('URL Shortener API is running ✅');
-});
-
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(process.env.PORT, () =>
     console.log(`Server running on port ${process.env.PORT}`)
